c77aaf9a76a4cd7a6611fd042018a9b629efdc06
dreedski <dreedski@hotmail.com>
2021-03-11 11:45:08 -0600

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,28 +1,22 @@
 package com.company;
+import java.util.Scanner;
 
-public class pyramid {
-    private int numRow;
+public class Main {
+    public static void main(String[] args) {
+        //pyramid pmid = new pyramid();
+        //^^^^ pyramid() needs an argument?
 
-    public pyramid(int numRow) {
-        //this.rows=rows;
-        this.numRow = numRow;  //make numRow public
+        Scanner in = new Scanner(System.in);
 
-        //make three different triangles...outer loops and L and R half?
+        System.out.print("How many rows do you want in your pyramid: 1-10? ");
+        int numRows = in.nextInt();
+        if (numRows < 1 || numRows > 10){ // || is an or operator
+            System.out.println("ERROR: Number must be greater than zero and less than 11");
 
-        for (int x=1; x<=numRow; x++) {
-            for (int y=1; y<=(numRow-x) * 2; y++) {
-                System.out.print(" ");
-            }
-            //make the L side of the pmid
-            for (int z=x; z>=1; z--) {
-                System.out.print(" " + z);
-            }
-            //make the R side of the pmid
-            for (int l=2; l<=x; l++) {
-                System.out.print(" " + l);
-            }
-            System.out.println();
-
+        } else {
+            pyramid pmid = new pyramid(numRows);
         }
+
+
     }
 }
